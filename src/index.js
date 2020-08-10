69596579f6cb6471b9f943ba8294a97a5c9861a8
grebenuks <grebenuks2406@gmail.com>
2020-08-10 20:34:15 +0300

    Делает 10 дз

--- src/index.js
+++ src/index.js
@@ -1,26 +1,52 @@
 import './styles.css';
-const body = document.querySelector('body');
-const checkboxOne = document.querySelector('.checkbox-one');
-const checkboxTwo = document.querySelector('.checkbox-two');
-console.log(checkboxOne);
-console.log(checkboxTwo);
+import data from './menu.json';
+import itemTemplates from './templates/item.hbs';
 
-checkboxOne.addEventListener('click', () => {
-  let darkTheme = checkboxOne.checked ? true : false;
-  localStorage.setItem('theme', darkTheme);
-  checkboxTwo.checked = darkTheme;
+const refs = {
+  menu: document.querySelector('.js-menu'),
+  items: itemTemplates(data),
+  body: document.querySelector('body'),
+  switchDiv: document.querySelector('.switch'),
+  switchInput: document.querySelector('.js-switch-input'),
+};
 
-  localStorage.getItem('theme') === 'true'
-    ? body.classList.add('background-black')
-    : body.classList.remove('background-black');
-});
+refs.menu.insertAdjacentHTML('afterbegin', refs.items);
 
-checkboxTwo.addEventListener('click', () => {
-  let darkTheme = checkboxTwo.checked ? true : false;
-  localStorage.setItem('theme', darkTheme);
-  checkboxOne.checked = darkTheme;
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
-  localStorage.getItem('theme') === 'true'
-    ? body.classList.add('background-black')
-    : body.classList.remove('background-black');
-});
+refs.switchDiv.addEventListener('click', changeTheme);
+
+function changeTheme() {
+  if (refs.switchInput.checked) {
+    darkTheme();
+    localStorage.setItem('Theme:', Theme.DARK);
+  } else {
+    lightTheme();
+    localStorage.setItem('Theme:', Theme.LIGHT);
+  }
+}
+
+function darkTheme() {
+  refs.switchInput.checked = true;
+  refs.body.classList.add('dark-theme');
+  refs.body.classList.remove('light-theme');
+}
+
+function lightTheme() {
+  refs.body.classList.add('light-theme');
+  refs.body.classList.remove('dark-theme');
+}
+
+getTheme();
+function getTheme() {
+  const themeCheck = localStorage.getItem('Theme:');
+  if (themeCheck === null || themeCheck === 'light-theme') {
+    localStorage.setItem('Theme:', Theme.LIGHT);
+    lightTheme();
+  } else if (themeCheck === 'dark-theme') {
+    darkTheme();
+  }
+}
---
+++ src/practice.js
@@ -0,0 +1,24 @@
+// import './styles.css';
+// const body = document.querySelector('body');
+// const checkboxOne = document.querySelector('.checkbox-one');
+// const checkboxTwo = document.querySelector('.checkbox-two');
+
+// checkboxOne.addEventListener('click', () => {
+//   let darkTheme = checkboxOne.checked ? true : false;
+//   localStorage.setItem('theme', darkTheme);
+//   checkboxTwo.checked = darkTheme;
+
+//   localStorage.getItem('theme') === 'true'
+//     ? body.classList.add('background-black')
+//     : body.classList.remove('background-black');
+// });
+
+// checkboxTwo.addEventListener('click', () => {
+//   let darkTheme = checkboxTwo.checked ? true : false;
+//   localStorage.setItem('theme', darkTheme);
+//   checkboxOne.checked = darkTheme;
+
+//   localStorage.getItem('theme') === 'true'
+//     ? body.classList.add('background-black')
+//     : body.classList.remove('background-black');
+// });
