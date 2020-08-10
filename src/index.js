5cd368742b3e835da8f932fbb0e4192a531d835d
grebenuks <grebenuks2406@gmail.com>
2020-08-10 17:16:41 +0300

    делает задачу

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './styles.css';
+const body = document.querySelector('body');
+const checkboxOne = document.querySelector('.checkbox-one');
+const checkboxTwo = document.querySelector('.checkbox-two');
+console.log(checkboxOne);
+console.log(checkboxTwo);
+
+checkboxOne.addEventListener('click', () => {
+  let darkTheme = checkboxOne.checked ? true : false;
+  localStorage.setItem('theme', darkTheme);
+  checkboxTwo.checked = darkTheme;
+
+  localStorage.getItem('theme') === 'true'
+    ? body.classList.add('background-black')
+    : body.classList.remove('background-black');
+});
+
+checkboxTwo.addEventListener('click', () => {
+  let darkTheme = checkboxTwo.checked ? true : false;
+  localStorage.setItem('theme', darkTheme);
+  checkboxOne.checked = darkTheme;
+
+  localStorage.getItem('theme') === 'true'
+    ? body.classList.add('background-black')
+    : body.classList.remove('background-black');
+});
